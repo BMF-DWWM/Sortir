86cb2e909590a7abc3d5e1611571f83772565c3b
CharlesDivaret <91057507+CharlesDivaret@users.noreply.github.com>
2021-11-15 18:05:07 +0100

    Formulaire Creation d'etat en BDD et formulaire creation de sortie ( ne fonctionnne pas en bdd)

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
+use App\Entity\Sortie;
+use App\Form\CreateEtatType;
+use App\Form\CreateSortieType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
     /**
-     * @Route("/sortie", name="sortie")
+     * @Route("/sortie/create", name="sortie_create")
      */
-    public function index(): Response
+    public function create(Request $request,
+                           EntityManagerInterface $entityManager
+    ): Response
     {
-        return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
+        $sortie = new Sortie();
+        $createSortieForm = $this->createForm(CreateSortieType::class,$sortie);
+        $createSortieForm->handleRequest($request );
+
+        if ($createSortieForm->isSubmitted()&&$createSortieForm->isValid()){
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+            $this->addFlash('success','Sortie Added ! Good job.');
+        }
+        return $this->render('sortie/create.html.twig',[
+            'createSortieForm' => $createSortieForm->createView()
         ]);
     }
 }
