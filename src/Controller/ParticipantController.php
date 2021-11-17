f46b61fcfee45cdb7c822781802c6501e626ec23
MrBolux <83412944+MrBolux@users.noreply.github.com>
2021-11-17 15:01:54 +0100

    view profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class ParticipantController extends AbstractController
 {
     /**
-     * @Route("/participant", name="participant_view")
+     * @Route("/participant/{id}", name="participant_view")
      */
-    public function view(): Response
+    public function view(Participant $participant): Response
     {
         return $this->render('participant/index.html.twig', [
             'controller_name' => 'ParticipantController',
