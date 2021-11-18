417c07874f9d615205056f1be8d63e45937f0a2e
MrBolux <83412944+MrBolux@users.noreply.github.com>
2021-11-18 11:52:40 +0100

    profile

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Form\RegistrationFormType;
+use App\Security\AppAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class ParticipantController extends AbstractController
 {
@@ -16,6 +21,41 @@ class ParticipantController extends AbstractController
     {
         return $this->render('participant/index.html.twig', [
             'controller_name' => 'ParticipantController',
+        ]);
+    }
+
+    /**
+     * @Route("/participant/{id}/modifier", name="participant_modifier")
+     */
+    public function register(Request $request,
+                             UserPasswordHasherInterface $passwordHasher,
+                             UserAuthenticatorInterface $authenticator,
+                             AppAuthenticator $appAuthenticator
+    ): Response
+    {
+        $user = $this ->getUser();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('main_home');
+        }
+
+        return $this->render('participant/index.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
