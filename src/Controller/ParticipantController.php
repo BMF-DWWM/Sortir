2f2f3bcf5c44b158410e34ab24fa83daa9cfd1c9
CharlesDivaret <91057507+CharlesDivaret@users.noreply.github.com>
2021-11-18 11:56:20 +0100

    123

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -45,7 +45,7 @@ class ParticipantController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+        //Surprise MotherFucker
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
